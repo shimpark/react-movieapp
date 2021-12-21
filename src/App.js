96bec61b198ce61cec02bf3356dde3f762a939ab
shimjaewoon <shimpark@gmail.com>
2021-12-22 03:30:01 +0900

    app.js 수정

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Detail from "./routes/Detail";
 
 
 function App() {
-    return (<Router>
+    return (<Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home/>} />          
         <Route path="/movie/:id" element={<Detail/>} />
