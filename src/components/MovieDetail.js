7ecd7fd6b5dea6e09712aa0c54f34ca9be58dd61
shimjaewoon <shimpark@gmail.com>
2021-12-22 03:08:58 +0900

    first

---
+++ src/components/MovieDetail.js
@@ -0,0 +1,36 @@
+import PropTypes from "prop-types";
+import{    
+    Link
+  } from "react-router-dom";
+import styles from "./Movie.module.css";
+
+
+function MovieDetail({ id, coverImg, title, year, summary, genres }) {
+  return (
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+MovieDetail.propTypes = {
+    id: PropTypes.number.isRequired,
+    coverImg: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  };
+
+export default MovieDetail;
---
+++ src/routes/Detail.js
@@ -0,0 +1,47 @@
+import { useParams } from "react-router-dom";
+import {useEffect, useState} from 'react';
+import MovieDetail from "../components/MovieDetail";
+
+function Detail() {
+
+
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
+
+    const {id} = useParams();
+    console.log(id);
+
+    var getMovie = async () => {
+        const json = await (
+            await fetch('https://yts.mx/api/v2/movie_details.json?movie_id=' + id)
+        ).json();
+        console.log(json);
+        setMovie(json.data.movie);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getMovie();
+    }, []);
+
+    return (
+        <div>
+          {loading ? (
+            <h1>Loading...</h1>
+          ) : (
+            <div>
+              <MovieDetail 
+                key={movie.id} 
+                id={movie.id} 
+                year={movie.year} 
+                coverImg={movie.medium_cover_image} 
+                title={movie.title} 
+                summary={movie.description_full} 
+                genres={movie.genres} />
+            </div>
+          )}
+        </div>
+      );
+}
+
+export default Detail;
